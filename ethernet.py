a33a999db057d12af34a9f172371bbae7ba509ca
Eric Van Albert <eric@van.al>
2015-01-21 21:56:15 -0500

    improving things

--- ethernet.py
+++ ethernet.py
@@ -6,6 +6,8 @@ ETH_P_ALL = 3
 import socket
 import select
 import queue
+import subprocess
+import driver
 
 class Ethernet(object):
     alice_nic="enp0s20u3u2"
@@ -13,20 +15,18 @@ class Ethernet(object):
 
     SNAPLEN=65536
 
-    @staticmethod
-    def attach(nic):
-        sock = socket.socket(socket.AF_PACKET,socket.SOCK_RAW,socket.htons(ETH_P_ALL))
-        sock.bind((nic,0))
-        sock.setblocking(0)
-        return sock
-
-    def __init__(self, alice_fn, bob_fn, debug=True):
+    def __init__(self, alice_fn, bob_fn, debug=True, tap=True):
         self.alice_fn = alice_fn
         self.bob_fn = bob_fn
         self.debug = debug
 
         self.a_w = queue.Queue()
         self.b_w = queue.Queue()
+
+        if tap:
+            self.tap=driver.Tap()
+        else:
+            self.tap=None
 
     def alice_write(self, data):
         self.a_w.put(data)
@@ -35,50 +35,66 @@ class Ethernet(object):
         self.b_w.put(data)
 
     def run(self):
-        alice_sock = self.attach(self.alice_nic)
-        bob_sock = self.attach(self.bob_nic)
+        def attach(nic):
+            result = subprocess.call(["ifconfig",nic,"up","promisc"])
+            if result:
+                raise Exception("ifconfig {0} return exit code {1}".format(nic,result))
+            sock = socket.socket(socket.AF_PACKET,socket.SOCK_RAW,socket.htons(ETH_P_ALL))
+            sock.bind((nic,0))
+            sock.setblocking(0)
+            return sock
 
-        while True:
-            to_w=[]
-            if not self.a_w.empty():
-                to_w.append(alice_sock)
-            if not self.b_w.empty():
-                to_w.append(bob_sock)
+        alice_sock = attach(self.alice_nic)
+        bob_sock = attach(self.bob_nic)
 
-            r,w,e=select.select([alice_sock,bob_sock],to_w,[alice_sock,bob_sock])
+        if self.tap:
+            self.tap.mitm()
 
-            if alice_sock in r:
-                a=alice_sock.recv(self.SNAPLEN)
-                if self.debug:
-                    print("ALICE:",' '.join([hex(c) for c in a]))
-                self.alice_fn(a, self.alice_write, self.bob_write)
-            if bob_sock in r:
-                b=bob_sock.recv(self.SNAPLEN)
-                if self.debug:
-                    print("BOB:",' '.join([hex(c) for c in b]))
-                self.bob_fn(b, self.bob_write, self.alice_write)
-            if alice_sock in w:
-                while True:
-                    try:
-                        wd = self.a_w.get_nowait()
-                        self.a_w.task_done()
-                    except queue.Empty:
-                        break
-                    print("Alice write:", len(wd))
-                    l=alice_sock.send(wd)
-            if bob_sock in w:
-                while True:
-                    try:
-                        wd = self.b_w.get_nowait()
-                        self.b_w.task_done()
-                    except queue.Empty:
-                        break
-                    print("Bob write:", len(wd))
-                    l=bob_sock.send(wd)
-            if alice_sock in e:
-                raise "ALICE EXCEPTION"
-            if alice_sock in e:
-                raise "BOB EXCEPTION"
+        try:
+            while True:
+                to_w=[]
+                if not self.a_w.empty():
+                    to_w.append(alice_sock)
+                if not self.b_w.empty():
+                    to_w.append(bob_sock)
+
+                r,w,e=select.select([alice_sock,bob_sock],to_w,[alice_sock,bob_sock])
+
+                if alice_sock in r:
+                    a=alice_sock.recv(self.SNAPLEN)
+                    if self.debug:
+                        print("ALICE:",' '.join([hex(c) for c in a]))
+                    self.alice_fn(a, self.alice_write, self.bob_write)
+                if bob_sock in r:
+                    b=bob_sock.recv(self.SNAPLEN)
+                    if self.debug:
+                        print("BOB:",' '.join([hex(c) for c in b]))
+                    self.bob_fn(b, self.bob_write, self.alice_write)
+                if alice_sock in w:
+                    while True:
+                        try:
+                            wd = self.a_w.get_nowait()
+                            self.a_w.task_done()
+                        except queue.Empty:
+                            break
+                        print("Alice write:", len(wd))
+                        l=alice_sock.send(wd)
+                if bob_sock in w:
+                    while True:
+                        try:
+                            wd = self.b_w.get_nowait()
+                            self.b_w.task_done()
+                        except queue.Empty:
+                            break
+                        print("Bob write:", len(wd))
+                        l=bob_sock.send(wd)
+                if alice_sock in e:
+                    raise "ALICE EXCEPTION"
+                if alice_sock in e:
+                    raise "BOB EXCEPTION"
+        finally:
+            if self.tap:
+                self.tap.passthru()
 
 # receive a packet
 #while True:
