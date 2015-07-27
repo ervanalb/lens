07c1a6ba9f29db33696ba50e22772119db9981de
Zach Banks <zbanks@mit.edu>
2015-07-27 19:38:16 -0400

    More test scripts

--- src/run_cloud2butt.py
+++ src/run_cloud2butt.py
@@ -14,11 +14,10 @@ import tornado.gen as gen
 from base import l, connect, NetLayer
 
 if __name__ == "__main__":
-    addr = "192.168.0.15"
     alice_nic = "enp0s20u3"
     bob_nic =   "enp0s20u4"
-    #addr = "192.168.1.10"
-    print "Capturing traffic to:", addr
+
+    print "Cloud 2 butt: s/cloud/butt/g over HTTP"
 
     tap = driver.FakeTap()
 
@@ -28,17 +27,12 @@ if __name__ == "__main__":
     eth_layer = ethernet.EthernetLayer()
     link_layer.register_child(eth_layer)
 
-    ipv4_layer = ip.IPv4Layer(addr_filter=[addr])
+    #ipv4_layer = ip.IPv4Layer(addr_filter=["192.168.1.10"])
+    ipv4_layer = ip.IPv4Layer(addr_filter=None)
     eth_layer.register_child(ipv4_layer, dpkt.ethernet.ETH_TYPE_IP)
 
-    udp_layer = udp.UDPLayer()
-    ipv4_layer.register_child(udp_layer, dpkt.ip.IP_PROTO_UDP)
-
-    ssh_filter_layer = tcp.TCPPassthruLayer(ports=[22])
-    ipv4_layer.register_child(ssh_filter_layer, dpkt.ip.IP_PROTO_TCP)
-
-    tcp_layer = tcp.TCPLayer(debug=False)
-    ssh_filter_layer.register_child(tcp_layer)
+    tcp_layer = tcp.TCPLayer(debug=True)
+    ipv4_layer.register_child(tcp_layer, dpkt.ip.IP_PROTO_TCP)
 
     http_lbf_layer = base.LineBufferLayer()
     tcp_layer.register_child(http_lbf_layer, 8000)
@@ -49,15 +43,6 @@ if __name__ == "__main__":
 
     c2b_layer = base.CloudToButtLayer()
     http_layer.register_child(c2b_layer, "text")
-
-    img_layer = http.ImageFlipLayer()
-    http_layer.register_child(img_layer, "image")
-
-    xss_layer = http.XSSInjectorLayer()
-    http_layer.register_child(xss_layer, "javascript")
-
-    #video_layer = udp.UDPVideoLayer()
-    #udp_layer.register_child(video_layer)
 
     try:
         loop.start()
